428d3f1aa057ef10346094569d43626fa0894449
Vinh <vinhdesu123@gmail.com>
2022-03-30 15:00:38 +0700

    vinhdesu123@gmail.com

--- android_app/lib/models/account.dart
+++ android_app/lib/models/account.dart
@@ -5,17 +5,17 @@ class AccountModel {
 
   AccountModel({this.address, this.mnemonic, this.privateKey});
 
+  AccountModel.fromJson(Map<String, dynamic> json) {
+    address = json['address'];
+    mnemonic = json['mnemonic'];
+    privateKey = json['privateKey'];
+  }
+
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['address'] = address;
     data['mnemonic'] = mnemonic;
+    data['privateKey'] = privateKey;
     return data;
   }
-
-  AccountModel.fromJson(Map<String, dynamic> json) {
-    address = json['address'];
-    mnemonic = json['mnemonic'];
-    return data;
-  }
-
 }
--- android_app/lib/models/item_post.dart
+++ android_app/lib/models/item_post.dart
@@ -3,14 +3,16 @@ class ItemPost {
   String? name;
   int? price;
   String? owner;
+  String? picture;
 
-  ItemPost({this.id, this.name, this.price, this.owner});
+  ItemPost({this.id, this.name, this.price, this.owner, this.picture});
 
   ItemPost.fromJson(Map<String, dynamic> json) {
     id = json['_id'];
     name = json['name'];
     price = json['price'];
     owner = json['owner'];
+    picture = json['picture'];
   }
 
   Map<String, dynamic> toJson() {
@@ -19,6 +21,7 @@ class ItemPost {
     data['name'] = name;
     data['price'] = price;
     data['owner'] = owner;
+    data['picture'] = picture;
     return data;
   }
 }
